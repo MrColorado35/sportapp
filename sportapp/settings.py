f1ccae6bc7ce09f32e5df5460712a2987aa4af55
MrColorado35 <stan.kaczorkiewicz@yahoo.co.uk>
2020-08-28 16:08:01 +0000

    Heroku connected to AWS

--- sportapp/settings.py
+++ sportapp/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'home',
 
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -163,3 +164,12 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if "USE_AWS" in os.environ:
+    # AWS Bucket configs
+    AWS_STORAGE_BUCKET_NAME = "one-sport-app"
+    AWS_S3_REGION_NAME = 'eu-west-2'
+    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
